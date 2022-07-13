4ce52126d077bef2ffbbab5e3cd8b89eec4617ed
igor.rabelo <igor.rabelo@coopersystem.com.br>
2022-07-13 17:35:41 -0300

    iniciando projeto alura-pizza frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.css'
+import Prato from './componentes/Prato';
+
 
 function App() {
+
+  const [pratos, setPratos] = useState([])
+
+  useEffect(() => {
+    fetch('https://alura-pizzaria.herokuapp.com/api/pratos/')
+    .then(resposta => resposta.json())
+    .then(dados => {
+      setPratos(dados)
+    })
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='banner-container'>
+        <img src="/imagens/banner.png" alt="Um banner mostrando uma pizza"/>
+      </div>
+      
+      <div className="logo-container">
+        <img src="/imagens/logo.png" alt="Logo da Alura Pizza"/>
+      </div>
+      <section className='cardapio'>
+        {pratos.map(prato => 
+        (<Prato 
+          key={prato.id} 
+          nome={prato.nome} 
+          descricao={prato.descricao} 
+          imagem={prato.imagem}
+          preco={prato.preco}
+        />))}
+      </section>
     </div>
   );
 }
